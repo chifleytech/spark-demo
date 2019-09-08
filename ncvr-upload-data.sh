e7e5714d7f84005275e222d117d04cca5ddbeb36
michael-walker <mwalker009@hotmail.com>
2019-09-08 18:51:44 +0900

    updated hostname

--- ncvr-upload-data.sh
+++ ncvr-upload-data.sh
@@ -2,6 +2,6 @@
 hdfs dfs -mkdir /data/ncvr
 hdfs dfs -mkdir /data/ncvr/2014
 hdfs dfs -mkdir /data/ncvr/2017
-hadoop fs -put /data/ncvr/VR_Snapshot_20141104.txt hdfs://master:9000/data/ncvr/2014/data.txt
-hadoop fs -put /data/ncvr/VR_Snapshot_20171107.txt hdfs://master:9000/data/ncvr/2017/data.txt
+hadoop fs -put /data/ncvr/VR_Snapshot_20141104.txt hdfs://$HOSTNAME:9000/data/ncvr/2014/data.txt
+hadoop fs -put /data/ncvr/VR_Snapshot_20171107.txt hdfs://$HOSTNAME:9000/data/ncvr/2017/data.txt
 date
